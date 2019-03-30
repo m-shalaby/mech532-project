7a5a1ada4f4e94362d8132055a15afb4bc30071a
Shalaby <mohammed.shalaby@mail.mcgillca>
2019-03-30 13:28:29 -0400

    Added Reynold calculations in MATLAB, added temporary NACA-0012 airfoil to XFLR

--- matlab/calculations.m
+++ matlab/calculations.m
@@ -1,16 +1,15 @@
 %%INPUTS
 %external conditions
-
 g = 9.81;
 rho = 1.225; %assume constant
-
+mu = 1.81E-5;
 
 %aircraft properties
-
 m = 2.32;
 % J = ;
-S_wing = 2*0.18;
-
+b_wing = 2;
+c_wing = 0.18;
+S_wing = b_wing*c_wing;
 
 
 %% LEVEL FLIGHT
@@ -20,24 +19,21 @@ V_lf = 20;
 alt_lf = 100;
 
 %Results
-
 L_lf = m*g;
 CL_lf = L_lf/(0.5*rho*V_lf^2*S_wing)
-
+Re_lf = rho*V_lf*c_wing/mu
 
 
 %% BANKED TURN
 
 %Banked Turn conditions
-
 phi = pi/4;
 
 %Results
-
-N = 1/cos(phi);
+N_bt = 1/cos(phi);
 L_bt = m*g/cos(phi)
-V = sqrt(2*L_bt/(rho*S_wing*CL_lf))
-
+V_bt = sqrt(2*L_bt/(rho*S_wing*CL_lf))
+Re_bt = rho*V_bt*c_wing/mu
 
 
 %% CLIMB
