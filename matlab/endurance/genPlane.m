a5e5b731ccd9f2629c02b0a501416b6ea2130aa0
Felix Lamy <felix.lamy@gmail.com>
2019-04-05 19:37:18 -0400

    Fixed Ah conversion

--- matlab/endurance/genPlane.m
+++ matlab/endurance/genPlane.m
@@ -21,7 +21,7 @@ plane.motor.n = 1;
 
 %battery parameters
 plane.batt.Vnom = 14.8;     %nominal battery voltage [V]
-plane.batt.totalQ = 7920;    %total battery capacity [As]
+plane.batt.totalQ = 7920000;    %total battery capacity [As]
 plane.batt.safetyQ = 0.05*plane.batt.totalQ;
 
 
