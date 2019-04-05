3b853a94800a05cfdf97d68558f53d6351033a80
Felix Lamy <felix.lamy@gmail.com>
2019-04-05 16:09:32 -0400

    fminsearch step 1

--- matlab/endurance/optimizeClimbparam.m
+++ matlab/endurance/optimizeClimbparam.m
@@ -1,33 +1,12 @@
-function [outClimbParam, outMapEnergy] = optimizeClimbparam(plane, total_climb, map_param)
+function [outClimbParam] = optimizeClimbparam(plane)
 %optimizeClimbparam finds the best operating parameters for maximising
 %climb endurance.
 
-%map climb energy to operating parameters
-mapEnergy = zeros(length(map_param.v),length(map_param.theta),length(map_param.alpha));
-for l = 1:length(map_param.v)
-    for m = 1:length(map_param.theta)
-        for n = 1:length(map_param.alpha)
-            climb_states = [map_param.v(l), map_param.theta(m), map_param.alpha(n)];
-            climb = Climb(plane,climb_states,total_climb);
-            if climb.flag == 1
-                mapEnergy(l,m,n) = 1E9;
-                continue
-            else
-            mapEnergy(l,m,n) = climb.totalEnergy;
-            end
-        end
-    end
-end
+x0 = [10; 10; -1];
+f = @(climb_states,plane)ClimbEnergy(climb_states,plane);
+ClimbParam = fminsearch(@(climb_states) f(climb_states,plane),x0);
 
-%find climbing states for minimum energy
-[~,I] = min(mapEnergy(:));
-[I,J,K] = ind2sub(size(mapEnergy),I);
-climb_v = map_param.v(I);
-climb_theta = map_param.theta(J);
-climb_alpha = map_param.alpha(K);
+outClimbParam = ClimbParam;
 
-%output
-outClimbParam = [climb_v;climb_theta;climb_alpha];
-outMapEnergy = mapEnergy;
 end
 
