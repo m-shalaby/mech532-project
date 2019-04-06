20305483576e442b3fcb2e9940a9c2fd2f85f83a
Felix Lamy <felix.lamy@gmail.com>
2019-04-06 14:14:18 -0400

    Added folders

--- matlab/endurance/SteadyFlight.m
+++ matlab/endurance/SteadyFlight.m
@@ -6,14 +6,14 @@ function [outSteadyFlight] = SteadyFlight(plane,deltaQ)
 v = plane.test.v;
 
 %capacity
-CL = (plane.m*9.81)/(0.5*plane.test.rho*plane.S*(v^2));
-sflight.alpha = calcAlpha(plane,CL);
+sflight.CL = (plane.m*9.81)/(0.5*plane.test.rho*plane.S*(v^2));
+sflight.alpha = calcAlpha(plane,sflight.CL);
 sflight.CT = calcClimbCT(plane, [v,0,sflight.alpha]);
 sflight.thrust = calcPropellerThrust(plane,sflight.CT);
 sflight.eta = calcEta(plane,sflight.CT);
 sflight.power = sflight.thrust*v/(sflight.eta*plane.motor.eff);
-sflight.distanceA = 0.5*deltaQ*v/sflight.power;
-sflight.distanceB = 0.5*deltaQ*v/sflight.power;
+sflight.distanceA = 0.5*deltaQ*v*plane.batt.Vnom/sflight.power;
+sflight.distanceB = 0.5*deltaQ*v*plane.batt.Vnom/sflight.power;
 sflight.distance = sflight.distanceA+sflight.distanceB;
 sflight.deltaQ = deltaQ;
 %time
